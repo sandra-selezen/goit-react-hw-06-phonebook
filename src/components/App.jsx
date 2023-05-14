931375d8f442d53f06affcc99a6baa9fbb30a3a2
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-05-14 20:06:08 +0300

    Adds titles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,9 @@ export const App = () => {
 
   return (
     <Container>
+      <h1>Phonebook</h1>
       <ContactForm />
+      <h2>Contacts</h2>
       <Filter />
       {contacts.length > 0 && <ContactList />}
       <Toaster position="top-center" />
